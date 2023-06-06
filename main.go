d6e06864465a432ce9d5c29a46269a76e7b1df91
XxjoacohenselxX <135734983+XxjoacohenselxX@users.noreply.github.com>
2023-06-06 19:36:24 -0300

    tarea tp4 go+wails

--- main.go
+++ main.go
@@ -1,32 +1,36 @@
 package main
 
-type errType string
+import (
+	"embed"
 
-const constErr errType = "blah"
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
 
-func (et errType) Error() string {
-	return string(et)
-}
-
-var errVar error = errType("foo")
-
-func genErr(i int) error {
-	switch i {
-	case 0:
-		return constErr
-	case 1:
-		return errVar
-	default:
-		return nil
-	}
-}
-
-func unreachable() {
-	err := errVar // @whicherrs func-dead "err"
-	_ = err
-}
+//go:embed all:frontend/src
+var assets embed.FS
 
 func main() {
-	err := genErr(0) // @whicherrs localerrs "err"
-	_ = err
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "miproyecto",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
 }
